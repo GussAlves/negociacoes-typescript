5029b13725f886bbcfa54e37c94d8dec75c17ac5
Gustavo Alves <50598580+GussAlves@users.noreply.github.com>
2023-01-03 01:55:34 -0300

    Criando controller para realizar validação do formulario

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,6 +1,6 @@
 export class Negociacao {
     constructor(data, quantidade, valor) {
-        this._data = valor;
+        this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
